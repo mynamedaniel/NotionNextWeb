5aa83fc59492eb26be4a3c03939b3f46c9ed9666
mynamedaniel <zhexuap@gmail.com>
2023-04-09 06:29:21 +0800

    Update config_next.js

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -5,9 +5,9 @@ const CONFIG_NEXT = {
 
   NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
-  POST_LIST_COVER: false, // 文章列表显示封面图
-  POST_LIST_PREVIEW: true, // 显示文章预览
-  POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
+  POST_LIST_COVER: true, // 文章列表显示封面图
+  POST_LIST_PREVIEW: false, // 显示文章预览
+  POST_LIST_SUMMARY: true, // 显示用户自定义摘要，有预览时优先只展示预览
 
   POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
@@ -16,7 +16,7 @@ const CONFIG_NEXT = {
   RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
   RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
-  RIGHT_AD: false, // 右侧广告
+  RIGHT_AD: true, // 右侧广告
 
   // 菜单
   MENU_HOME: true, // 显示首页
@@ -30,7 +30,7 @@ const CONFIG_NEXT = {
   WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
   WIDGET_TOC: true, // 移动端显示悬浮目录
 
-  ARTICLE_SHARE: process.env.NEXT_PUBLIC_ARTICLE_SHARE || false, // 文章分享功能
+  ARTICLE_SHARE: process.env.NEXT_PUBLIC_ARTICLE_SHARE || true, // 文章分享功能
   ARTICLE_RELATE_POSTS: true, // 相关文章推荐
   ARTICLE_COPYRIGHT: true // 文章版权声明
 
