172f1f83c6b17e42a3886cea081470e13443254f
mynamedaniel <zhexuap@gmail.com>
2023-05-02 17:42:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'cookiepapa', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '终身学习者，长期主义者，保守主义者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'xuzheblog.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '数据分析,数字营销,管理,阅读,健身' // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '数据分析,数字营销,管理,阅读,健身', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'zhexuap@gmail.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
