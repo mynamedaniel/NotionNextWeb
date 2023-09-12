2f43c5561fac1db310612147344433f17c1dde94
cookiepapa <zhexuap@gmail.com>
2023-09-12 11:40:50 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2023-04-15', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://xuzheblog.com' },
+    { title: '欢迎下载心晴App', url: 'https://apps.apple.com/cn/app/%E5%BF%83%E6%99%B4-%E8%AE%B0%E5%BD%95%E5%BF%83%E6%83%85-%E6%8E%92%E8%A7%A3%E6%83%85%E7%BB%AA-%E8%A7%A3%E7%AD%94%E9%97%AE%E9%A2%98/id6463860072' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: '分享数据',
+  HERO_TITLE_2: '与个人思考',
+  HERO_TITLE_3: 'XUZHEBLOG.COM',
+  HERO_TITLE_4: '',
+  HERO_TITLE_5: '数据分析师的自我修养',
+  HERO_TITLE_LINK: 'https://www.xuzheblog.com/article/340027af-0364-46f5-b2a3-f7807ada37aa',
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -29,14 +29,13 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '🔍 Cookiepapa',
+    '📊 数据分析师',
+    '💻 独立开发者',
+    '👶 前全职奶爸',
+    '🐶 铲屎官X2'
   ],
-  INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  INFO_CARD_URL: 'https://xuzheblog.com', // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
@@ -98,7 +97,7 @@ const CONFIG = {
     }
   ],
 
-  SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   SOCIAL_CARD_TITLE_1: '交流频道',
   SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   SOCIAL_CARD_TITLE_3: '点击加入社群',
